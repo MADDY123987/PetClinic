61b31b28ea6a4bbe49fa5f9d8a4012c140b19151
Madhavan <madhavan20041@gmail.com>
2026-06-02 23:56:02 +0530

    Entity for Admin,Patient,Veterinarian Created

---
+++ src/main/java/com/Universal/PetClinic/Model/Admin.java
@@ -0,0 +1,15 @@
+package com.Universal.PetClinic.Model;
+
+
+public class Admin {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private String userType;
+    private boolean isEnabled;
+
+}
---
+++ src/main/java/com/Universal/PetClinic/Model/Patient.java
@@ -0,0 +1,13 @@
+package com.Universal.PetClinic.Model;
+
+public class Patient {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private String userType;
+    private boolean isEnabled;
+}
